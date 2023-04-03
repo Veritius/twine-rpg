78859407a15726c97c315c42c66a60b777df7c79
Veritius <veritiusgaming@gmail.com>
2023-04-04 09:51:17 +1000

    Fix time

--- story/meta/script/time.js
+++ story/meta/script/time.js
@@ -5,13 +5,17 @@ The <<advancetime x>> macro takes one variable, X, which is a number representin
 
 Macro.add('advancetime', {
     handler: function() {
+        if (this.args.length < 1) {
+            return this.error("No amount of time to advance by passed");
+        }
+
         // Coerce first argument to number.
         let amount = this.args[0] *= 1;
-        let time = State.getVar("$%%TimeIncrementor");
+        let time = State.variables["%%timeIncrementor"];
         let vtime = time + amount;
         let increments = vtime / 60;
         let remainder = vtime % 60;
-        State.getVar("$%%TimeIncrementor") = remainder;
+        State.variables["%%timeIncrementor"] = remainder;
 
         Cycle.get("seasons").update(increments);
         Cycle.get("months").update(increments);
